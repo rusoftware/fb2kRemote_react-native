65594c0add92dbd23535f24a58c0c3d80282b05b
Fede Teiserskis <federico.teiserskis@ordergroove.com>
2023-06-12 15:41:06 -0300

    Do not change album cover if album is the same

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import Main from './src/Main';
 // - parse *.cue files if possible in explorer view
 // - Add settings screen to add Foobar2000 ip address
 // - Check how to connect via wifi without the need to add the IP
+// - setSelectedPlaylist(updateData.player.activeItem.playlistId) -->> lo que sucede es que no se puede trabajar con la selected playlist, pero de lo contrario, no se actualiza la lista de reproducción actual en home... Que hacemos?
 
 export default function App() {
   return <Main />
--- src/Main.js
+++ src/Main.js
@@ -34,6 +34,7 @@ const Main = () => {
   const [showToast, setShowToast] = useState(false)
 
   const currentPositionRef = useRef(songPosition)
+  const prevAlbumRef = useRef(currentSong.album)
 
   const params = {
     player: true,
@@ -307,10 +308,13 @@ const Main = () => {
       } catch (error) {
         console.error('Error:', error)
       }
+    };
+  
+    if (currentSong.album && prevAlbumRef.current !== currentSong.album) {
+      getCoverArt()
+      prevAlbumRef.current = currentSong.album
     }
-
-    getCoverArt();
-  }, [currentSong])
+  }, [currentSong.album])
 
   useEffect(() => {
     const blockedPlaylists = ['Full Albums', 'Search']
@@ -385,8 +389,10 @@ const Main = () => {
     const handleUpdatesMessage = update => {
       const updateData = JSON.parse(update.data)
       if (updateData && updateData.player && updateData.player.activeItem) {
+        //console.log(updateData)
         drawSongInfo(updateData)
         handleVolume(updateData.player.volume)
+        setSelectedPlaylist(updateData.player.activeItem.playlistId)
       }
     }
 
